7a57deddf524cc19ba4908a14940ff9e4b2cdd0c
Varun Nair <nairv@purdue.edu>
2019-05-28 01:16:20 -0400

    Coeff Testing

--- wdt1.py
+++ wdt1.py
@@ -1,17 +1,24 @@
 import numpy as np
 import scipy as sp
 import cv2
-import matplotlib as mtplib
-import pywt as wt
+import matplotlib as plt
+import pywt
 
 img = cv2.imread("DSC1.jpeg", 0)
 imgA = np.asarray(img)
 
-print(imgA.shape)
+
 #print(type(imgA))
 #print(imgA)
-coeffs = wt.dwt2(imgA, 'haar')
-cA, (cH, cV, cD) = coeffs
+coeffs = pywt.dwt2(imgA, 'haar')
+cA, (cH, cV, cD) = coeffs #cA:          cV:         cD:         cH:         .
+coeffsA = np.asarray(coeffs)
 
-print(cA)
-print(cV)
+print("cA =", cA)
+print("cV =", cV)
+print(coeffsA)
+
+print(type.coeffs)
+print("image shape",imgA.shape)
+print("cA shape:",cA.shape)
+print("cD shape", cD.shape)

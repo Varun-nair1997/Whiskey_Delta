68088b10b44eedd90d986d731848b06b0ddf70f7
Varun Nair <nairv@purdue.edu>
2019-05-28 13:42:39 -0400

    update 3
    implemented VISUshrink
    using db5

--- wdt1.py
+++ wdt1.py
@@ -7,12 +7,54 @@ import pywt
 img = cv2.imread("DSC1.jpeg", 0)
 imgA = np.asarray(img)
 
+#Adding Noise
+row,col= imgA.shape #ch
+mean = 0
+var = 0.1
+sigma = var**0.5
+gauss = np.random.normal(mean,sigma,(row,col))#ch
+gauss = gauss.reshape(row,col)#ch
+noisy = imgA + gauss
+
 
 #print(type(imgA))
-#print(imgA)
-coeffs = pywt.dwt2(imgA, 'haar')
-cA, (cH, cV, cD) = coeffs #cA:          cV:         cD:         cH:         .
+#print("imgA=", imgA)
+print(noisy.shape)
+
+wavelet = pywt.Wavelet('db5')
+coeffs = pywt.dwt2(noisy, mode='constant',wavelet=wavelet)
+LL, (LH, HL, HH) = coeffs #cA:LL cV:LH cD:HH cH: HL.
 coeffsA = np.asarray(coeffs)
+#avg1 = np.average(imgA)
+#print(avg1)
+
+#VISUshrink
+TLL = np.sqrt((np.std(LL)**2)*(np.log(1428*2148)))
+THH = np.sqrt((np.std(HH)**2)*(np.log(1428*2148)))
+
+LL1 =pywt.threshold(LL, TLL/2, 'soft')
+# pywt.threshold(LH, 150, 'soft', substitute=0)
+# pywt.threshold(HL, 150, 'soft', substitute=0)
+HH1 = pywt.threshold(HH, THH/2, 'soft')
+
+
+inv = pywt.idwt(LL1, HH1, 'db5')
+
+print("LL",LL1.shape)
+print("HH",HH1.shape)
+print(inv.shape)
+
+
+cv2.imshow("noisy",(noisy))
+cv2.waitKey()
+
+cv2.imshow("denoised1",(inv))
+cv2.waitKey()
+
+"""
+cv2.imshow("denoised3",(inv))
+cv2.waitKey()
+
 
 print("cA =", cA)
 print("cV =", cV)
@@ -22,3 +64,4 @@ print(type.coeffs)
 print("image shape",imgA.shape)
 print("cA shape:",cA.shape)
 print("cD shape", cD.shape)
+"""
--- wt_testing.py
+++ wt_testing.py
@@ -2,8 +2,8 @@ import pywt
 import numpy as np
 
 data = np.ones((4,4),dtype=np.float64)
-#wavelet = pywt.Wavelet('shan')
-wavelet = pywt.ContinuousWavelet('shan1.5-1.0')
+wavelet = pywt.Wavelet('db3')
+#wavelet = pywt.ContinuousWavelet('fbsp')
 coeffs = pywt.dwt2(data, 'coif1')
 cA,(cH,cV,cD) = coeffs
 
@@ -20,4 +20,11 @@ print(cD)
 print("-")
 print("size", cA.size, cH.size, cV.size, cD.size)
 print("-")
-print(wavelet)
+#print(wavelet)
+#print(pywt.wavelist())
+
+phi, psi, x = wavelet.wavefun(level=2)
+print(phi)
+print(psi)
+a = phi.reshape((21,1))
+print(a)

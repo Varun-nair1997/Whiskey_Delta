9cc6caf9b80b271f56e503ad00b859b11338b00f
Varun Nair <nairv@purdue.edu>
2019-06-02 12:42:44 -0400

    update 7
    cleaned up wdt1

--- wdt1.py
+++ wdt1.py
@@ -5,10 +5,12 @@ import matplotlib as plt
 import pywt
 import skimage
 
-img = cv2.imread("DSC4.JPG",0)
+
+img = cv2.imread("DSC1.jpeg",0)
 imgA = np.asarray(img)
 # imgC = cv2.equalizeHist(imgB)
 # imgA = np.stack((imgB,imgB))
+
 #Adding Noise
 row,col = imgA.shape #ch
 mean = 0
@@ -16,54 +18,67 @@ var = 0.5
 sigma = var**0.5
 gauss = np.random.normal(mean,sigma,(row,col))#ch
 gauss = gauss.reshape(row,col)#ch
-noisy = imgA + gauss
+#noisy = imgA+gauss
 
+
+filterdim = imgA.shape
+#enhancing brightness
+alpha = 1.1
+beta = 10
+filter2 = np.zeros(filterdim,imgA.dtype)
+for y in range(filterdim[0]):
+    for x in range(filterdim[1]):
+        filter2[y, x] = np.clip(alpha * imgA[y, x] + beta, 0, 255)
+
+noisy = filter2
 
 #print(type(imgA))
 #print("imgA=", imgA)
 print(noisy.shape)
 
+#wavelet transform
 wavelet = pywt.Wavelet('haar')
 coeffs = pywt.dwt2(noisy, mode='constant',wavelet=wavelet)
 LL, (LH, HL, HH) = coeffs #cA:LL cV:LH cD:HH cH: HL.
 coeffsA = np.asarray(coeffs)
-#avg1 = np.average(imgA)
-#print(avg1)
 
-sizeLL = LL.shape
+
+sizeLL = imgA.shape
 number= sizeLL[0]*sizeLL[1]
 sizeimg = imgA.shape
 simgx=sizeimg[0]
 simgy=sizeimg[1]
+
 #VISUshrink
 TLL = np.sqrt((np.std(LL)**2)*(np.log(number)))
 THH = np.sqrt((np.std(HH)**2)*(np.log(number)))
 
-tscale = 5/8
+tscale = 1/3
 LL1 =pywt.threshold(LL, TLL*tscale, 'soft')
-# pywt.threshold(LH, 150, 'soft', substitute=0)
-# pywt.threshold(HL, 150, 'soft', substitute=0)
 HH1 = pywt.threshold(HH, THH*tscale, 'soft')
+
+#inverse transform
 inv = pywt.idwt(LL1, HH1, wavelet=wavelet)
-HHscaled = cv2.resize(HH1, (simgy,simgx))
-LLscaled = cv2.resize(LL1, (simgy,simgx))
+
+#HHscaed = cv2.resize(HH1, (simgy,simgx))
+#LLscaled = cv2.resize(LL1, (simgy,simgx))
 invscaled = cv2.resize(inv, (simgy,simgx))
-imgscaled = cv2.resize(noisy, (simgy,simgx))
-# LLflat = np.ravel(LLscaled)
-# HHflat = np.ravel(HHscaled)
+#imgscaled = cv2.resize(noisy, (simgy,simgx))
 
-filter1=cv2.blur(invscaled,(5,5))
+filter1= invscaled #cv2.blur(invscaled,(5,5))
+
 filtered = cv2.imwrite('filter1.jpg',filter1)
-imgb = cv2.imread('filtered.jpg',0)
+imgb = cv2.imread('filter1.jpg',0)
 imgB = np.asarray(imgb)
+print(filterdim[1])
 
-#x = cv2.inpaint(imgA, imgB, 3, cv2.INPAINT_NS)
 
 #filtered = skimage.filters.wiener(inv,HH1)
 
 print("LL",LL1.shape)
 print("HH",HH1.shape)
 print(inv.shape)
+
 
 
 # cv2.imshow("noisy",(noisy))
@@ -76,19 +91,18 @@ print(inv.shape)
 # cv2.waitKey()
 #
 
-print(imgB)
 
-cv2.imshow("invscal",(invscaled))
-cv2.waitKey()
-
-cv2.imshow("inter",(filter1))
-cv2.waitKey()
-
-cv2.imshow("orignal",(imgscaled))
-cv2.waitKey()
-
-# cv2.imshow("psuedo final",(imgB))
+# cv2.imshow("invscal",(invscaled))
 # cv2.waitKey()
+
+cv2.imshow("abc",(imgA)) #original
+cv2.waitKey()
+#
+cv2.imshow("xyz",(imgB)) #denoised
+cv2.waitKey()
+
+cv2.imshow("pqr",(noisy)) #filter2
+cv2.waitKey()
 
 """
 print(type.coeffs)
